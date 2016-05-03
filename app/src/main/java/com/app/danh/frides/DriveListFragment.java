2afe88893999fdab5063c1ed77ac13ec58acf0b6
Steven Stulga <sjstulga@vt.edu>
2016-05-02 20:08:00 -0400

    Added list view to the driver portion of the app

--- app/src/main/java/com/app/danh/frides/DriveListFragment.java
+++ app/src/main/java/com/app/danh/frides/DriveListFragment.java
@@ -1,5 +1,6 @@
 package com.app.danh.frides;
 
+import android.support.annotation.Nullable;
 import android.support.v4.app.ListFragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,11 +11,17 @@ import android.view.ViewGroup;
  * Created by Danh on 5/2/2016.
  */
 public class DriveListFragment extends ListFragment {
+    RideRequestAdapter rideRequestAdapter;
+
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        rideRequestAdapter = new RideRequestAdapter(getContext(), ((DriverActivity) getActivity()).requestsList);
+        setListAdapter(rideRequestAdapter);
     }
 
+    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.rider_ride_list, container, false);
